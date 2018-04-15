6cf10e601c942bbf8581648671d269c2e96d55f3
nyceane <=>
2018-04-14 21:03:46 -0700

    merged arduino test with ncs sdk code

--- test_arduino.py
+++ test_arduino.py
@@ -4,4 +4,4 @@ arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=.1)
 while True:
 	data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
 	if data:
-		print(data)
+		print(data.decode("utf-8"))
